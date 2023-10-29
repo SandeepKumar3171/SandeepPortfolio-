f95eb98f54e65fb6bffa13f86052d5e58fcf105d
SandeepKumar3171 <sandeepidkk@gmail.com>
2023-10-29 22:09:52 +0530

    update project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,27 +3,25 @@ import React from 'react'
 const Footer = () => {
   return (
    <>
-    <div className='border p-2 h-fit flex flex-row justify-between'>
-      <div className='flex flex-col'>
+    <div className='border p-2 h-fit md:flex flex-row justify-around grid grid-cols-2 gap-5'>
+      <div className='flex flex-col items-center'>
+      <p>Footer</p>
+      <p>Footer</p>
+      <a href="http:///api.whatsapp.com/send?phone=9616403152" target="_blank" rel="noopener noreferrer">whatsapp</a>
+      </div>
+      <div className='flex flex-col items-center'>
       <p>Footer</p>
       <p>Footer</p>
       </div>
-      <div className='flex flex-col'>
+      <div className='flex flex-col items-center'>
       <p>Footer</p>
       <p>Footer</p>
       </div>
-      <div className='flex flex-col'>
+      <div className='flex flex-col items-center'>
       <p>Footer</p>
       <p>Footer</p>
       </div>
-      <div className='flex flex-col'>
-      <p>Footer</p>
-      <p>Footer</p>
-      </div>
-      <div className='flex flex-col'>
-      <p>Footer</p>
-      <p>Footer</p>
-      </div>
+     
     </div>
     </>
   )
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div>
+    <div className='p-5 flex'>
       <p>Project page</p>
+      <iframe title='project1' src="https://project-hard-code-api.vercel.app/" frameborder="0"></iframe>
+      <embed src="https://project-hard-code-api.vercel.app/" type="" />
     </div>
   )
 }
