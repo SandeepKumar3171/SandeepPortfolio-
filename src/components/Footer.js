bffac8a3601c62f18cfc3758012a1e0db3efa3ad
SandeepKumar3171 <sandeepidkk@gmail.com>
2023-10-29 21:18:30 +0530

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,27 @@ import React from 'react'
 const Footer = () => {
   return (
    <>
-    <div className='border h-fit'>
+    <div className='border p-2 h-fit flex flex-row justify-between'>
+      <div className='flex flex-col'>
       <p>Footer</p>
       <p>Footer</p>
+      </div>
+      <div className='flex flex-col'>
       <p>Footer</p>
       <p>Footer</p>
+      </div>
+      <div className='flex flex-col'>
       <p>Footer</p>
+      <p>Footer</p>
+      </div>
+      <div className='flex flex-col'>
+      <p>Footer</p>
+      <p>Footer</p>
+      </div>
+      <div className='flex flex-col'>
+      <p>Footer</p>
+      <p>Footer</p>
+      </div>
     </div>
     </>
   )
