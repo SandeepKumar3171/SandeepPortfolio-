3b7fc1940a2262fa357bd42ec4dd4ccbf9a7e127
SandeepKumar3171 <sandeepidkk@gmail.com>
2023-10-30 22:47:22 +0530

    update navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,9 +17,10 @@ const Home = () => {
           React.js.
         </p>
         <div className='flex flex-row gap-5 justify-center md:justify-start md:pl-5 items-center p-2 text-2xl'>
-          <a target='_blank' href='https://www.linkedin.com/in/sandeep-kashyap-336a69239/' className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce' ><FaLinkedin/></a>
-          <a target='_blank' href='https://github.com/SandeepKumar3171'  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce'><FaGithub/></a>
-          <a href="mailto:/sandeepidkk@gmail.com" target="_blank"  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce text-red-500'><BiLogoGmail/></a>
+          <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/sandeep-kashyap-336a69239/' className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce' ><FaLinkedin/></a>
+          <a target='_blank' rel="noreferrer" href='https://github.com/SandeepKumar3171'  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce'><FaGithub/></a>
+          <a href="mailto:/sandeepidkk@gmail.com" target="_blank" rel="noreferrer"  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce text-red-500'><BiLogoGmail/></a>
+          <button className='transition duration-150 ease-in-out '>click me</button>
         </div>
         </div>
         <div className='md:w-1/2 w-full p-5'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,13 +15,13 @@ const Navbar = () => {
     <div className='flex flex-row w-full gap-5 p-4 items-center text-center justify-between bg-black  text-lg font-semibold shadow-lg shadow-red-600'>
       <div className='flex flex-row md:gap-5 gap-2 items-center'>
        <Link to="/"><img className='w-12 rounded-full shadow-lg shadow-red-600' src={SandeepImg} alt="" /></Link>
-        <Link to="/" className='hover:text-blue-400 hover:underline'><p>Sandeep</p></Link>
+        <Link to="/" className='hover:text-blue-400 hover:animate-bounce'><p>Sandeep</p></Link>
       </div>
       <div className='md:flex hidden flex-row gap-5'>
-       <Link to="/" className='hover:text-blue-400 hover:underline '>Home</Link>
-        <Link to="/about" className='hover:text-blue-400 hover:underline'>About</Link>
-       <Link to="/project" className='hover:text-blue-400 hover:underline'>Project</Link>
-        <Link to="/contact" className='hover:text-blue-400 hover:underline'>Contact</Link>
+       <Link to="/" className='hover:text-blue-400 hover:animate-bounce'>Home</Link>
+        <Link to="/about" className='hover:text-blue-400  hover:animate-bounce'>About</Link>
+       <Link to="/project" className='hover:text-blue-400  hover:animate-bounce'>Project</Link>
+        <Link to="/contact" className='hover:text-blue-400  hover:animate-bounce'>Contact</Link>
       </div>
       <div className='md:hidden cursor-pointer' onClick={clickHandler}><p  className='text-2xl '><AiOutlineMenu/></p></div>
     </div>
