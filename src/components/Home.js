dd3e3d8fe0f4014043099533ff9669a2983b1560
SandeepKumar3171 <sandeepidkk@gmail.com>
2023-10-30 21:34:39 +0530

    update Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import SandeepImg from "./Images/sandeepImg.jpg"
+import { FaLinkedin,FaGithub } from 'react-icons/fa';
+import { BiLogoGmail } from 'react-icons/bi';
 
 const Home = () => {
   return (
@@ -14,6 +16,11 @@ const Home = () => {
           Node.js, Express.js, MongoDB, and frontend using
           React.js.
         </p>
+        <div className='flex flex-row gap-5 justify-center md:justify-start md:pl-5 items-center border text-2xl'>
+          <p><FaLinkedin/></p>
+          <p><FaGithub/></p>
+          <p><BiLogoGmail/></p>
+        </div>
         </div>
         <div className='md:w-1/2 w-full p-5'>
           <p className='rounded-full bg-red-400 border-2 border-blue-500 shadow-xl shadow-red-600'><img className='rounded-full p-2 ' src={SandeepImg} alt="" /></p>
