bc8516bf5250d74ddade82789d9c7b5f8a542935
SandeepKumar3171 <sandeepidkk@gmail.com>
2023-10-30 23:13:10 +0530

    update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,13 +14,12 @@ const Home = () => {
         <p className='text-start mt-6'>Dedicated MERN stack developer with expertise in
           dynamic web apps, excelling in both backend with
           Node.js, Express.js, MongoDB, and frontend using
-          React.js.
+          React.js. <abbr title="You can contact me">mail me</abbr>
         </p>
         <div className='flex flex-row gap-5 justify-center md:justify-start md:pl-5 items-center p-2 text-2xl'>
-          <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/sandeep-kashyap-336a69239/' className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce' ><FaLinkedin/></a>
-          <a target='_blank' rel="noreferrer" href='https://github.com/SandeepKumar3171'  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce'><FaGithub/></a>
-          <a href="mailto:/sandeepidkk@gmail.com" target="_blank" rel="noreferrer"  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce text-red-500'><BiLogoGmail/></a>
-          <button className='transition duration-150 ease-in-out '>click me</button>
+          <a title='go to my Linkedin page' target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/sandeep-kashyap-336a69239/' className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce' ><FaLinkedin/></a>
+          <a title='go to my github page' target='_blank' rel="noreferrer" href='https://github.com/SandeepKumar3171'  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce'><FaGithub/></a>
+          <a href="mailto:/sandeepidkk@gmail.com" target="_blank" rel="noreferrer" title='you can mail me'  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce text-red-500'><BiLogoGmail/></a>
         </div>
         </div>
         <div className='md:w-1/2 w-full p-5'>
