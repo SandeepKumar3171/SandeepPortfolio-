60715d15c2a3cda0514c0855e4d32b22ffa13ec7
SandeepKumar3171 <sandeepidkk@gmail.com>
2023-10-31 23:09:25 +0530

    update page

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div className='p-5 flex'>
+    <div className='p-5 flex flex-col'>
       <p>Project page</p>
-      <iframe title='project1' src="https://project-hard-code-api.vercel.app/" frameborder="0"></iframe>
-      <embed src="https://project-hard-code-api.vercel.app/" type="" />
+      <div className='grid grid-cols-2'>
+      <iframe className='w-full h-[400px]' title='project1' src="https://myapp2-tjfc.vercel.app/" frameborder="0"></iframe>
+      <iframe  title='project2' className='w-full h-[400px]' src="https://project-hard-code-api.vercel.app/" type="" />
+      </div>
+      
     </div>
   )
 }
