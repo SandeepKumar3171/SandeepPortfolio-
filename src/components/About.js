e1474f8a33e1f30bc19df9356a34386b42707a25
SandeepKumar3171 <sandeepidkk@gmail.com>
2023-11-01 22:20:58 +0530

    update contact page

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
     <div>
       <p>About page</p>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut mollitia sint veritatis facilis maxime modi ratione quos cum at. Aspernatur impedit beatae fuga ut culpa, debitis inventore quis dolore repellendus!</p>
-      <p className='text-2xl font-semibold text-center items-center m-5 text-red-600'>SKILLS</p>
+      <p className='text-2xl font-semibold underline text-center items-center m-5 text-red-600'>SKILLS</p>
       <div className='grid md:grid-cols-4 grid-cols-2 justify-evenly gap-5 p-5 border-t m-5 pl-20'>
         <p className='text-8xl text-red-500'><AiFillHtml5/></p>
         <p className='text-8xl text-red-500'><FaCss3Alt/></p>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FaLinkedin,FaGithub } from 'react-icons/fa';
+import { BiLogoGmail } from 'react-icons/bi';
 
 const Contact = () => {
   return (
-    <div>
-      <p>Contact Page</p>
+    <div className='flex flex-col justify-center'>
+      <p className='text-2xl font-semibold underline text-center items-center m-5 text-red-600'>Contact Me</p>
+      <div className='flex flex-row gap-5 justify-center md:pl-5 items-center p-2 text-2xl'>
+          <a title='go to my Linkedin page' target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/sandeep-kashyap-336a69239/' className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce' ><FaLinkedin/></a>
+          <a title='go to my github page' target='_blank' rel="noreferrer" href='https://github.com/SandeepKumar3171'  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce'><FaGithub/></a>
+          <a href="mailto:/sandeepidkk@gmail.com" target="_blank" rel="noreferrer" title='you can mail me'  className='hover:text-blue-600 transform skew-y-12 animate-spin hover:animate-bounce text-red-500'><BiLogoGmail/></a>
+        </div>
     </div>
   )
 }
